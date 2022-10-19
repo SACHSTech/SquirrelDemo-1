2086127e36fff663b3f4fdcd16c20285fa364385
iydia <lydia.he23@ycdsbk12.ca>
2022-10-19 10:39:51 -0400

    commit 1

---
+++ src/OOPDemo/Squirrel.java
@@ -0,0 +1,48 @@
+package OOPDemo;
+
+public class Squirrel{
+    // instance variables
+    String colourString;
+    String sizeString;
+    int nutCountInt;
+    Boolean aliveBoolean;
+    Boolean iqDouble;
+
+    // constructor
+    /**
+     * FUCK THA JAVADOCS
+     * @param newColourString
+     * @param newSizeString
+     * @param newInt
+     * @param newNutCountInt
+     * @param newiqDouble
+     */
+    public Squirrel(String newColourString, String newSizeString, int newInt, int newNutCountInt, boolean newiqDouble) {
+        this.aliveBoolean = true;
+        this.colourString = newColourString;
+        this.sizeString = newSizeString;
+        this.nutCountInt = newNutCountInt;
+        this.iqDouble = newiqDouble;
+    }
+
+    // instance methods
+    public void run(){
+        System.out.println("run!!");
+    }
+
+    public void findNut(int numberNuts){
+        nutCountInt++;
+    }
+
+    public int getNutCount(){
+        return this.nutCountInt;
+    }
+
+    public void climb(){
+        System.out.println("Climb that tree!!!!");
+    }
+
+    public void kill(){
+        this.aliveBoolean = false;
+    }
+}
